a76d30ffc33d79f0a48a61dfcb914c7093009cb4
Imam541 <80622187+BADTze@users.noreply.github.com>
2024-03-25 08:47:58 +0700

    Page Fix

--- src/app/comp-1/page.tsx
+++ src/app/comp-1/page.tsx
@@ -69,7 +69,8 @@ export default function Areachart() {
     );
   };
   return (
-    <div className="px-3">
+    <div className="h-full px-3">
+
       <header className="flex justify-between mt-4">
         <Link className="max-w-sm" href="/">
           <h3 className="text-lg ml-5 font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
@@ -78,40 +79,59 @@ export default function Areachart() {
         </Link>
         <DatePicker className="max-w-sm" />
       </header>
-      <div className="mt-12">
-        <div className="flex">
-          <div className=" m-2 rounded-lg border w-40">
-            <p>Testing</p>
+
+      <div className=" flex mt-5 h-96 space-x-4 h-full">
+        <div className="border size-96 h-96 rounded-lg ">
+          Test
+        </div>
+
+        <div className="flex flex-col">
+          <div className="flex space-x-2 mb-3 h-40 rounded-lg h-1/2">
+            <Card className="w-40 h-40">Ceki Ceki</Card>
+            <div className="border rounded-lg max-h-40 w-full">
+              <AreaChart
+                className="h-40 w-auto"
+                data={chartdata}
+                index="date"
+                categories={["Running"]}
+                colors={["blue"]}
+                yAxisWidth={30}
+                customTooltip={customTooltip}
+              />
+            </div>
           </div>
-          <div className="m-2 rounded-lg border w-1/2">
-            <AreaChart
-              className="h-40 w-auto"
-              data={chartdata}
-              index="date"
-              categories={["Running"]}
-              colors={["blue"]}
-              yAxisWidth={30}
-              customTooltip={customTooltip}
-            />
+          <div className="flex space-x-2 mb-3 h-40 rounded-lg h-1/2">
+            <Card className="w-40 h-40">Ceki Ceki</Card>
+            <div className="border rounded-lg max-h-40 w-full">
+              <AreaChart
+                className="h-40 w-auto"
+                data={chartdata}
+                index="date"
+                categories={["Running"]}
+                colors={["blue"]}
+                yAxisWidth={30}
+                customTooltip={customTooltip}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-2 mb-3 h-40 rounded-lg h-1/2">
+            <Card className="w-40 h-40">Ceki Ceki</Card>
+            <div className="border rounded-lg max-h-40 w-full">
+              <AreaChart
+                className="h-40 w-auto"
+                data={chartdata}
+                index="date"
+                categories={["Running"]}
+                colors={["blue"]}
+                yAxisWidth={30}
+                customTooltip={customTooltip}
+              />
+            </div>
           </div>
         </div>
-        <div className="flex ">
-          <div className="m-2 rounded-lg border w-40">
-            <h1>Test</h1>
-          </div>
-          <div className="m-2 rounded-lg border w-1/2">
-            <AreaChart
-              className="h-40 w-auto"
-              data={chartdata}
-              index="date"
-              categories={["Running"]}
-              colors={["blue"]}
-              yAxisWidth={30}
-              customTooltip={customTooltip}
-            />
-          </div>
-        </div>
+        
       </div>
+
     </div>
   );
 }
