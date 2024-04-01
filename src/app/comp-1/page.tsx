0041f8dcb7ed1b8891784eb0c00c828b092dea1b
Imam541 <80622187+BADTze@users.noreply.github.com>
2024-04-01 13:47:40 +0700

    Editing Layout

--- src/app/comp-1/page.tsx
+++ src/app/comp-1/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { AreaChart, Card, DatePicker, Title } from "@tremor/react";
+import { Just_Me_Again_Down_Here } from "next/font/google";
 import Link from "next/link";
+import { hasUncaughtExceptionCaptureCallback } from "process";
 import {
   ReactElement,
   JSXElementConstructor,
@@ -11,7 +13,7 @@ import {
   useState,
 } from "react";
 import ReactApexChart from "react-apexcharts";
-
+import { Interface } from "readline/promises";
 export default function AreachartPage() {
   return (
     <div className="px-3 bg-transparent">
@@ -24,39 +26,98 @@ export default function AreachartPage() {
         <DatePicker className="max-w-sm" />
       </header>
 
-      <div className=" flex h-96 space-x-2 h-full">
-        <div className=" rounded-lg space-y-2">
-          <img src="kompresor-gas.jpg" className="rounded-lg border-2" />
-          <div className="bg-green-600 shadow-lg text-white text-2xl p-5 h-20 w-1/2">
-            <h1>Running</h1>
+      <div className=" grid grid-cols-3 gap-3 h-full">
+        <div className="col-span-1 rounded-lg space-y-2">
+          <img
+            src="kompresor-gas.jpg"
+            className="rounded-lg border-2 object-fit"
+          />
+          <div className="bg-green-600 shadow-lg text-white text-4xl text-center p-5 h-20">
+            Running
           </div>
-          <div className="h-12 border-2 shadow-lg bg-white p-2">RH</div>
-          <div className="h-12 border-2 shadow-lg bg-white p-2">RH period</div>
-          <div className="h-12 border-2 shadow-lg bg-white p-2">Down Time</div>
-          <div className="p-2 border-2 shadow-lg bg-white">
+          <div className="h-17 border-2 shadow-lg p-2">
+            <h2>Running Hours</h2>
+            <div className="text-3xl text-center">3000</div>
+          </div>
+          <div className="h-17 border-2 shadow-lg p-2">
+            <h2>Running Hours (Period)</h2>
+            <div className="text-3xl text-center">3000</div>
+          </div>
+          <div className="h-17 border-2 shadow-lg p-2">
+            <h2>Down Time (Minute)</h2>
+            <div className="text-3xl text-center">30</div>
+          </div>
+
+          <div className="p-2 border-2 shadow-lg">
             <h2>Maintenance</h2>
             <div className="flex justify-around">
-              <div className="text-green-600">PM = 30</div>
-              <div className="text-yellow-400">CM = 30</div>
-              <div className="text-red-600">EM = 30</div>
+              <div className="text-green-600 text-2xl">PM = 3 </div>
+              <div className="text-yellow-400 text-2xl">CM = 5</div>
+              <div className="text-red-500 text-2xl">EM = 6</div>
             </div>
+          </div>
+
+          <div className="p-2 border-2 shadow-lg h-96">
+            <ReactApexChart
+              options={{
+                dataLabels: {
+                  enabled: false,
+                },
+                plotOptions: {
+                  bar: {
+                    borderRadius: 4,
+                    horizontal: true,
+                  },
+                },
+                xaxis: {
+                  categories: [
+                    "JAN",
+                    "FEB",
+                    "MAR",
+                    "APR",
+                    "MEI",
+                    "JUN",
+                    "JUL",
+                    "AUG",
+                    "SEP",
+                    "OKT",
+                    "NOV",
+                    "DES",
+                  ],
+                  labels: {
+                    formatter: (val) => {
+                      return val;
+                    },
+                  },
+                },
+              }}
+              series={[
+                {
+                  name: "series1",
+                  data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                },
+              ]}
+              type="bar"
+              height={350}
+            />
           </div>
         </div>
 
-        <div className="flex flex-col w-full">
+        <div className="grid col-span-2">
           <div className="flex space-x-2 mb-2 h-40 rounded-lg h-1/2">
             <div className=" flex-col w-40 h-40 rounded-lg p-2 border-2 bg-white">
-              <div className="text-sm">Hot Side First Stage Air Temp <b>MAX 30</b></div>
-              <div className="mt-2 text-5xl text-center">300</div>
+              <p className="text-xs">Hot Side First Stage Air Temperature</p>
+              <b>MAX 200</b>
+              <div className="mt-2 text-5xl text-center">200</div>
               <div className="mt-2">Celcius</div>
             </div>
 
-            <div className="border-2 rounded-lg max-h-40 w-full bg-white">
-              {/* <ReactApexChart
+            <div className="border-2 rounded-lg max-h-40 w-[820px] bg-white">
+              <ReactApexChart
                 options={{
                   chart: {
                     height: 150,
-                    width:20,
+                    width: 20,
                     type: "area",
                   },
                   dataLabels: {
@@ -87,44 +148,266 @@ export default function AreachartPage() {
                   {
                     name: "series1",
                     data: [31, 40, 28, 51, 42, 109, 100],
-                  }
+                  },
                 ]}
                 type="area"
                 height={150}
-              /> */}
+              />
             </div>
           </div>
           <div className="flex space-x-2 mb-2 h-40 rounded-lg h-1/2">
             <div className=" flex-col w-40 h-40 rounded-lg p-2 border-2 bg-white">
-              <div className="text-sm">Cold Side First Stage Air Temp <b>MAX 30</b></div>
-              <div className="mt-2 text-5xl text-center">300</div>
+              <p className="text-xs">Cold Side First Stage Air Temperature</p>
+              <b>MAX 55</b>
+              <div className="mt-2 text-5xl text-center">55</div>
               <div className="mt-2">Celcius</div>
             </div>
 
-            <div className="border-2 rounded-lg max-h-40 w-full bg-white">
-            
+            <div className="border-2 rounded-lg max-h-40 w-[820px] bg-white">
+              <ReactApexChart
+                options={{
+                  chart: {
+                    height: 150,
+                    width: 20,
+                    type: "area",
+                  },
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  stroke: {
+                    curve: "smooth",
+                  },
+                  xaxis: {
+                    type: "datetime",
+                    categories: [
+                      "2018-09-19T00:00:00.000Z",
+                      "2018-09-19T01:30:00.000Z",
+                      "2018-09-19T02:30:00.000Z",
+                      "2018-09-19T03:30:00.000Z",
+                      "2018-09-19T04:30:00.000Z",
+                      "2018-09-19T05:30:00.000Z",
+                      "2018-09-19T06:30:00.000Z",
+                    ],
+                  },
+                  tooltip: {
+                    x: {
+                      format: "dd/MM/yy HH:mm",
+                    },
+                  },
+                }}
+                series={[
+                  {
+                    name: "series1",
+                    data: [31, 40, 28, 51, 42, 109, 100],
+                  },
+                ]}
+                type="area"
+                height={150}
+              />
             </div>
           </div>
           <div className="flex space-x-2 mb-2 h-40 rounded-lg h-1/2">
-            <div className=" flex-col w-41 h-40 rounded-lg p-2 border-2 bg-white">
-              <div className="text-sm">Hot Side Second Stage Air Temperature <b>MAX 30</b></div>
-              <div className="mt-2 text-5xl text-center">300</div>
+            <div className=" flex-col w-40 h-40 rounded-lg p-2 border-2 bg-white">
+              <p className="text-xs">Hot Side Second Stage Air Temperature</p>
+              <b>MAX 175</b>
+              <div className="mt-2 text-5xl text-center">100</div>
               <div className="mt-2">Celcius</div>
             </div>
 
-            <div className="border-2 rounded-lg max-h-40 w-full bg-white">
-            
+            <div className="border-2 rounded-lg max-h-40 w-[820px] bg-white">
+              <ReactApexChart
+                options={{
+                  chart: {
+                    height: 150,
+                    width: 20,
+                    type: "area",
+                  },
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  stroke: {
+                    curve: "smooth",
+                  },
+                  xaxis: {
+                    type: "datetime",
+                    categories: [
+                      "2018-09-19T00:00:00.000Z",
+                      "2018-09-19T01:30:00.000Z",
+                      "2018-09-19T02:30:00.000Z",
+                      "2018-09-19T03:30:00.000Z",
+                      "2018-09-19T04:30:00.000Z",
+                      "2018-09-19T05:30:00.000Z",
+                      "2018-09-19T06:30:00.000Z",
+                    ],
+                  },
+                  tooltip: {
+                    x: {
+                      format: "dd/MM/yy HH:mm",
+                    },
+                  },
+                }}
+                series={[
+                  {
+                    name: "series1",
+                    data: [31, 40, 28, 51, 42, 109, 100],
+                  },
+                ]}
+                type="area"
+                height={150}
+              />
             </div>
           </div>
           <div className="flex space-x-2 mb-2 h-40 rounded-lg h-1/2">
-            <div className=" flex-col w-41 h-40 rounded-lg p-2 border-2 bg-white">
-              <div className="text-sm">Hot Side Second Stage Air Temperature <b>MAX 30</b></div>
-              <div className="mt-2 text-5xl text-center">300</div>
+            <div className=" flex-col w-40 h-40 rounded-lg p-2 border-2 bg-white">
+              <p className="text-xs">Cold Side Second Stage Air Temperature</p>
+              <b>MAX 55</b>
+              <div className="mt-2 text-5xl text-center">55</div>
               <div className="mt-2">Celcius</div>
             </div>
 
-            <div className="border-2 rounded-lg max-h-40 w-full bg-white">
-               
+            <div className="border-2 rounded-lg max-h-40 w-[820px] bg-white">
+              <ReactApexChart
+                options={{
+                  chart: {
+                    height: 150,
+                    width: 20,
+                    type: "area",
+                  },
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  stroke: {
+                    curve: "smooth",
+                  },
+                  xaxis: {
+                    type: "datetime",
+                    categories: [
+                      "2018-09-19T00:00:00.000Z",
+                      "2018-09-19T01:30:00.000Z",
+                      "2018-09-19T02:30:00.000Z",
+                      "2018-09-19T03:30:00.000Z",
+                      "2018-09-19T04:30:00.000Z",
+                      "2018-09-19T05:30:00.000Z",
+                      "2018-09-19T06:30:00.000Z",
+                    ],
+                  },
+                  tooltip: {
+                    x: {
+                      format: "dd/MM/yy HH:mm",
+                    },
+                  },
+                }}
+                series={[
+                  {
+                    name: "series1",
+                    data: [31, 40, 28, 51, 42, 109, 100],
+                  },
+                ]}
+                type="area"
+                height={150}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-2 mb-2 h-40 rounded-lg h-1/2">
+            <div className=" flex-col w-40 h-40 rounded-lg p-2 border-2 bg-white">
+              <p className="text-xs">Hot Side Third Stage Air Temperature</p>
+              <b>MAX 165</b>
+              <div className="mt-2 text-5xl text-center">150</div>
+              <div className="mt-2">Celcius</div>
+            </div>
+
+            <div className="border-2 rounded-lg max-h-40 w-[820px] bg-white">
+              <ReactApexChart
+                options={{
+                  chart: {
+                    height: 150,
+                    width: 20,
+                    type: "area",
+                  },
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  stroke: {
+                    curve: "smooth",
+                  },
+                  xaxis: {
+                    type: "datetime",
+                    categories: [
+                      "2018-09-19T00:00:00.000Z",
+                      "2018-09-19T01:30:00.000Z",
+                      "2018-09-19T02:30:00.000Z",
+                      "2018-09-19T03:30:00.000Z",
+                      "2018-09-19T04:30:00.000Z",
+                      "2018-09-19T05:30:00.000Z",
+                      "2018-09-19T06:30:00.000Z",
+                    ],
+                  },
+                  tooltip: {
+                    x: {
+                      format: "dd/MM/yy HH:mm",
+                    },
+                  },
+                }}
+                series={[
+                  {
+                    name: "series1",
+                    data: [31, 40, 28, 51, 42, 109, 100],
+                  },
+                ]}
+                type="area"
+                height={150}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-2 mb-2 h-40 rounded-lg h-1/2">
+            <div className=" flex-col w-40 h-40 rounded-lg p-2 border-2 bg-white">
+              <p className="text-xs">Cold Side Third Stage Air Temperature</p>
+              <b>MAX 55</b>
+              <div className="mt-2 text-5xl text-center">45</div>
+              <div className="mt-2">Celcius</div>
+            </div>
+
+            <div className="border-2 rounded-lg max-h-40 w-[820px] bg-white">
+              <ReactApexChart
+                options={{
+                  chart: {
+                    height: 150,
+                    width: 20,
+                    type: "area",
+                  },
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  stroke: {
+                    curve: "smooth",
+                  },
+                  xaxis: {
+                    type: "datetime",
+                    categories: [
+                      "2018-09-19T00:00:00.000Z",
+                      "2018-09-19T01:30:00.000Z",
+                      "2018-09-19T02:30:00.000Z",
+                      "2018-09-19T03:30:00.000Z",
+                      "2018-09-19T04:30:00.000Z",
+                      "2018-09-19T05:30:00.000Z",
+                      "2018-09-19T06:30:00.000Z",
+                    ],
+                  },
+                  tooltip: {
+                    x: {
+                      format: "dd/MM/yy HH:mm",
+                    },
+                  },
+                }}
+                series={[
+                  {
+                    name: "series1",
+                    data: [31, 40, 28, 51, 42, 109, 100],
+                  },
+                ]}
+                type="area"
+                height={150}
+              />
             </div>
           </div>
         </div>
